2047eba67815da2ab4ca23ceafe423f2e0bdfb3e
Aaron Cohen <aarondcohen@gmail.com>
2017-06-20 02:10:05 -0400

    Stage 5

---
+++ stage-5/solution.rb
@@ -0,0 +1,28 @@
+class Player
+  MAX_HEALTH = 20
+
+  def initialize
+    @health = MAX_HEALTH
+  end
+
+  def play_turn(warrior)
+    if warrior.feel.captive?
+      warrior.rescue!
+    elsif warrior.feel.enemy?
+      warrior.attack!
+    elsif taking_damage? warrior
+      warrior.walk!
+    elsif warrior.health < MAX_HEALTH
+      warrior.rest!
+    else
+      warrior.walk!
+    end
+
+    @health = warrior.health
+  end
+
+  def taking_damage?(warrior)
+    warrior.health < @health
+  end
+end
+
